f8619c58a444f32ad711d22917efad90824f26e4
Barry Russell <blrussell09@gmail.com>
2018-02-27 19:47:09 -0600

    Added Challenge 1

--- arrays.js
+++ arrays.js
@@ -1,1 +1,13 @@
 console.log("wassup mutha fukka")
+
+// Challenge 1
+var test1 = 'otto'
+var testSplit = test1.split('').reverse().join('')
+// var testRev = testSplit;
+// var newArr = testRev;
+
+if (test1 === testSplit) {
+  console.log('true')
+} else {
+  console.log('false')
+};

28a8c9118d798b72862eb798665e0a35c9b0be91
Barry Russell <blrussell09@gmail.com>
2018-02-27 21:18:21 -0600

    Challenge 2

--- arrays.js
+++ arrays.js
@@ -11,3 +11,20 @@ if (test1 === testSplit) {
 } else {
   console.log('false')
 };
+
+// Challenge 2
+
+var book1 = "Catch 22"
+var book2 = "Slaughterhouse 5"
+var book3 = "Fahrenheit 451"
+var bookArr = [];
+
+var arr = book3.split('')
+for (var j=0; j<arr.length; j++) {
+  if (+arr[j]) {
+  console.log(arr[j]);
+  bookArr.push(arr[j]);
+  }
+}
+
+console.log(bookArr);
